dbf70cb5b58f96811f7ebd0f3e51e636b5fbaf69
Iris <iris.souza@zup.com.br>
2021-04-20 14:44:03 -0300

    Adicionar método cadastrar categoria

---
+++ src/main/java/com/br/zup/marketing/controllers/CategoriaController.java
@@ -0,0 +1,23 @@
+package com.br.zup.marketing.controllers;
+
+import com.br.zup.marketing.dtos.categoria.CadastroCategoriaDTO;
+import com.br.zup.marketing.entities.Categoria;
+import com.br.zup.marketing.services.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("categorias/")
+public class CategoriaController {
+
+    @Autowired
+    private CategoriaService categoriaService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Categoria cadastrarCategoria(@RequestBody CadastroCategoriaDTO categoriaDTO){
+        Categoria categoria = categoriaService.cadastrarCategoria(categoriaDTO.converterDTOParaModel());
+        return categoria;
+    }
+}
