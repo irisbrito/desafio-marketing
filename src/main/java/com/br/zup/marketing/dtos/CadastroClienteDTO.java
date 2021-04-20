f03f960819ed1ad689d404e62f8a55dba48dfc47
Iris <iris.souza@zup.com.br>
2021-04-20 14:21:49 -0300

    Adicionar cadastroClienteDTO

---
+++ src/main/java/com/br/zup/marketing/dtos/CadastroClienteDTO.java
@@ -0,0 +1,61 @@
+package com.br.zup.marketing.dtos;
+
+import com.br.zup.marketing.entities.Cliente;
+import com.br.zup.marketing.entities.Produto;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+
+public class CadastroClienteDTO {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Email(message = "Email inválido")
+    private String email;
+    @NotNull(message = "Nome completo é obrigatório")
+    private String nomeCompleto;
+    @NotNull(message = "O telefone é obrigatório")
+    private String telefone;
+    private List<Produto> produtos;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Cliente converterDTOParaModel(){
+        Cliente cliente = new Cliente();
+        cliente.setId(this.id);
+        cliente.setEmail(this.email);
+        cliente.setNomeCompleto(this.nomeCompleto);
+        cliente.setTelefone(this.telefone);
+        cliente.setProdutos(this.produtos);
+
+        return cliente;
+    }
+}
