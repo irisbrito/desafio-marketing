0a66c37729f8b13cb4026bf3c1dc319a026b0fd3
Iris <iris.souza@zup.com.br>
2021-04-20 13:23:32 -0300

    Criar projeto

---
+++ src/test/java/com/br/zup/marketing/MarketingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.zup.marketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
