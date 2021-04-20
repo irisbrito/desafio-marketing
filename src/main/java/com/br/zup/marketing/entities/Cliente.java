2895eaeb4425bc9885002b34f5242d6c7496e195
Iris <iris.souza@zup.com.br>
2021-04-20 13:39:49 -0300

    Criar entidade Cliente

---
+++ src/main/java/com/br/zup/marketing/entities/Cliente.java
@@ -0,0 +1,52 @@
+package com.br.zup.marketing.entities;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String email;
+    private String nomeCompleto;
+    private String telefone;
+
+    @OneToMany
+    private List<Produto> produtos;
+
+    public Cliente() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
+    }
+}
