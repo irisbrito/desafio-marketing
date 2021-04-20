82f3d2caf3441bafe88f6361fcfdb8adb50c051e
Iris <iris.souza@zup.com.br>
2021-04-20 13:47:22 -0300

    Adicionar categoriaRepository

---
+++ src/main/java/com/br/zup/marketing/repositories/CategoriaRepository.java
@@ -0,0 +1,7 @@
+package com.br.zup.marketing.repositories;
+
+import com.br.zup.marketing.entities.Categoria;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoriaRepository extends CrudRepository<Categoria, Integer> {
+}
