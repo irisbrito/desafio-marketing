4b20ddd8a855e5ec0e009a9292df0a732e732915
Iris <iris.souza@zup.com.br>
2021-04-22 18:49:32 -0300

    Criar método customizado para categoria: findByNome

--- src/main/java/com/br/zup/marketing/repositories/CategoriaRepository.java
+++ src/main/java/com/br/zup/marketing/repositories/CategoriaRepository.java
@@ -3,5 +3,8 @@ package com.br.zup.marketing.repositories;
 import com.br.zup.marketing.entities.Categoria;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoriaRepository extends CrudRepository<Categoria, Integer> {
+    Optional<Categoria> findByNome(String nome);
 }
