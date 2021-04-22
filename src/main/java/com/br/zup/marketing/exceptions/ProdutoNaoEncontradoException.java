dc6d579ffbd6611e6a202ecb75d27f3c53bbcc81
Iris <iris.souza@zup.com.br>
2021-04-22 13:25:26 -0300

    Adicionar classe de exceção produto não encontrado

---
+++ src/main/java/com/br/zup/marketing/exceptions/ProdutoNaoEncontradoException.java
@@ -0,0 +1,44 @@
+package com.br.zup.marketing.exceptions;
+
+public class ProdutoNaoEncontradoException extends RuntimeException{
+    private int Status = 400;
+    private String campo = "nome";
+    private String razaoDoErro = "Bad request";
+    private String tipoDoErro = "Produto não cadastrado";
+
+    public ProdutoNaoEncontradoException(String message) {
+        super(message);
+    }
+
+    public int getStatus() {
+        return Status;
+    }
+
+    public void setStatus(int status) {
+        Status = status;
+    }
+
+    public String getCampo() {
+        return campo;
+    }
+
+    public void setCampo(String campo) {
+        this.campo = campo;
+    }
+
+    public String getRazaoDoErro() {
+        return razaoDoErro;
+    }
+
+    public void setRazaoDoErro(String razaoDoErro) {
+        this.razaoDoErro = razaoDoErro;
+    }
+
+    public String getTipoDoErro() {
+        return tipoDoErro;
+    }
+
+    public void setTipoDoErro(String tipoDoErro) {
+        this.tipoDoErro = tipoDoErro;
+    }
+}
