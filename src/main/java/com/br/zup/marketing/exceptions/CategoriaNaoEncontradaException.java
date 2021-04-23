fd5a8df781f1b56fdd0b01cdf28c5c4998191dbf
Iris <iris.souza@zup.com.br>
2021-04-22 22:34:52 -0300

    Adicionar classe de exceção categoria não encontrada

---
+++ src/main/java/com/br/zup/marketing/exceptions/CategoriaNaoEncontradaException.java
@@ -0,0 +1,44 @@
+package com.br.zup.marketing.exceptions;
+
+public class CategoriaNaoEncontradaException extends RuntimeException{
+    private int Status = 400;
+    private String campo = "id";
+    private String razaoDoErro = "Bad request";
+    private String tipoDoErro = "Categoria não cadastrada";
+
+    public CategoriaNaoEncontradaException(String message) {
+        super(message);
+    }
+
+    public int getStatus() {
+        return Status;
+    }
+
+    public void setStatus(int status) {
+        Status = status;
+    }
+
+    public String getCampo() {
+        return campo;
+    }
+
+    public void setCampo(String campo) {
+        this.campo = campo;
+    }
+
+    public String getRazaoDoErro() {
+        return razaoDoErro;
+    }
+
+    public void setRazaoDoErro(String razaoDoErro) {
+        this.razaoDoErro = razaoDoErro;
+    }
+
+    public String getTipoDoErro() {
+        return tipoDoErro;
+    }
+
+    public void setTipoDoErro(String tipoDoErro) {
+        this.tipoDoErro = tipoDoErro;
+    }
+}
