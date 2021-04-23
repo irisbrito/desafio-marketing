64594653a56d58a62858194588259ddd89e2a0d5
Iris <iris.souza@zup.com.br>
2021-04-22 22:03:01 -0300

    Refatorar assinatura do método findByEmail para Optional

--- src/main/java/com/br/zup/marketing/repositories/ClienteRepository.java
+++ src/main/java/com/br/zup/marketing/repositories/ClienteRepository.java
@@ -3,12 +3,14 @@ package com.br.zup.marketing.repositories;
 import com.br.zup.marketing.entities.Cliente;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface ClienteRepository extends CrudRepository<Cliente, Integer> {
 
     Iterable<Cliente> findAllByProdutos_id(int id);
     Iterable<Cliente> findAllByProdutosNome(String nome);
     Iterable<Cliente> findAllByProdutos_categorias_id(int id);
     Iterable<Cliente> findAllByProdutosCategoriasNome(String nome);
-    Iterable<Cliente> findByEmail(String email);
+    Optional<Cliente> findByEmail(String email);
     boolean existsByEmail(String email);
 }
